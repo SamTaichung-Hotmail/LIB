eb24276b9907f9f4815b1a14407a1638136c5b3c
Sam Chang <sam.taichung@hotmail.com>
2021-09-16 12:30:18 +0800

    Update:Add Modbus Function

---
+++ Modbus.h
@@ -0,0 +1,114 @@
+﻿
+#pragma once
+#ifdef AUTOMALIB_EXPORTS
+#define AUTOMALIB_API __declspec(dllexport)
+#else
+#define AUTOMALIB_API __declspec(dllimport)
+#endif
+#include "Protocol.h"				
+#include "ComNet.h"			
+#include "ComPort.h"			
+#include "TimeCounter.h"
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <assert.h>
+#include <limits.h>
+
+typedef unsigned char U8;
+typedef short I16;
+typedef unsigned short U16;
+typedef long I32;
+typedef unsigned long U32;
+typedef unsigned long long U64;
+typedef float F32;
+typedef enum {
+	xmfcNone,
+	xmfcReadCoil,
+	xmfcReadInputStatus,
+	xmfcReadHoldingRegister,
+	xmfcReadInputRegister,
+	xmfcForceSingleCoil,
+	xmfcPresentSingleRegister,
+	xmfcEcho = 0x08,
+	xmfcForceMultiCoil = 0x0F,
+	xmfcPresentMultiRegister = 0x10,
+}XModbusFuncCode;
+namespace AutoLib{
+AUTOMALIB_API class MModbus : public MProtocol
+{
+
+public:
+	enum ProtocolFormat {
+		pfRTU_CLIENT,
+		pfRTU_SERVER,
+		pfTCP_CLIENT,
+		pfTCP_SERVER,
+	};
+	static UINT DoThread(void *);
+	void Synchronize();
+	AUTOMALIB_API MModbus(MBase *pB,CString strID,CString strName);
+	AUTOMALIB_API ~MModbus();
+	AUTOMALIB_API bool Init();
+	AUTOMALIB_API bool StopSynchronize();	//停止通訊同步
+	AUTOMALIB_API void SetTCP(CString strIP, CString strPort);
+	AUTOMALIB_API void SetComport(int intComport, int intBaudRate, 
+		CString strParity,int intByteSize,int intStopBit);
+	AUTOMALIB_API void SetModbusID(byte MasterID,byte SlaveID);
+	AUTOMALIB_API void SetProtocol(ProtocolFormat Protocol);
+	AUTOMALIB_API bool isIDLE();
+	AUTOMALIB_API bool Open();
+	AUTOMALIB_API void InsertInputRegArray(ProtocolWordArray* pArray);
+	AUTOMALIB_API int GetCountOfInputRegArray();
+	AUTOMALIB_API void InsertHoldRegArray(ProtocolWordArray* pArray);
+	AUTOMALIB_API int GetCountOfHoldRegArray();
+	AUTOMALIB_API void InsertCoilBitArray(ProtocolBitArray* pArray);
+	AUTOMALIB_API int GetCountOfCoilBitArray();
+	AUTOMALIB_API void InsertInputBitArray(ProtocolBitArray* pArray);
+	AUTOMALIB_API int GetCountOfInputBitArray();
+	AUTOMALIB_API bool ReadInputRegArray(ProtocolWordArray* pArray);
+	AUTOMALIB_API bool ReadHoldRegArray(ProtocolWordArray* pArray);
+	AUTOMALIB_API bool WriteHoldRegArray(ProtocolWordArray* pArray);
+	AUTOMALIB_API bool WriteCoilArray(ProtocolBitArray* pArray);
+	AUTOMALIB_API bool ReadCoilArray(ProtocolBitArray* pArray);
+	AUTOMALIB_API bool ReadInputStatusArray(ProtocolBitArray* pArray);
+	MComPort *m_pComport;
+	MComNet *m_pTCPServer;
+	MComNet *m_pTCPClient;
+protected:
+	bool m_bWriteData;
+	std::vector<ProtocolWordArray*> m_InputWords;
+	std::vector<ProtocolWordArray*> m_HoldWords;
+	std::vector<ProtocolBitArray*> m_CoilBits;
+	std::vector<ProtocolBitArray*> m_InputBits;
+	ProtocolWordArray* m_pWordArray;
+	ProtocolBitArray* m_pBitArray;
+	ProtocolFormat m_Protocol;
+	XModbusFuncCode m_ActionCode;
+	U16 m_ReadCoilNum;
+	CTimeCounter m_TmCounter;
+	CString m_strServerIP, m_strServerPort;
+	DWORD m_pThread;
+	HANDLE m_hThread;
+	CRITICAL_SECTION m_csSynchronize;
+	bool m_bSynchronized;
+	int m_intArrayIndex;
+	int m_intParseCount;
+	int m_intRecvCount;
+	byte m_chSend[1024];
+	byte m_chRecv[1024];
+	bool m_bSkip;
+	WORD m_wdErrorCode;
+	U16 m_UID;
+private:
+	int m_intComport;
+	int m_intBaudRate;
+	CString m_strParity;
+	int m_intByteSize;
+	int m_intStopBit;
+	byte m_MasterID, m_SlaveID;
+	void Modbus_TCPServer(U8 SlaveID, byte* pRecv, int intRecv, byte* pSend, int* intSend);
+	void Modbus_COMServer(U8 SlaveID, byte* pRecv, int intRecv, byte* pSend, int* intSend);
+};
+}//namespace AutoLib
+
---
+++ Protocol.h
@@ -0,0 +1,100 @@
+﻿
+#pragma once
+#pragma warning( disable : 4091)
+#ifdef AUTOMALIB_EXPORTS
+#define AUTOMALIB_API __declspec(dllexport)
+#else
+#define AUTOMALIB_API __declspec(dllimport)
+#endif
+#include "Base.h"		
+#include "TimeCounter.h"
+#include "Communication.h"
+namespace AutoLib{ 
+AUTOMALIB_API class ProtocolDataArray
+{
+public:
+	enum ProtocolDataType {
+		pdt_WORD,
+		pdt_BIT,
+	};
+	AUTOMALIB_API ProtocolDataArray(
+		ProtocolDataType DataType,
+		unsigned long long ulStartAddress,//開始位址2000,3000...
+		unsigned long usAmount);
+	AUTOMALIB_API ProtocolDataType GetType() { return m_DataType; };
+	AUTOMALIB_API void SetStartAddr(unsigned long long Addr) { m_ulStartAddress=Addr; };
+	AUTOMALIB_API unsigned long long GetStartAddr() { return m_ulStartAddress; };
+	AUTOMALIB_API unsigned long GetAmount() { return m_usAmount; };
+	AUTOMALIB_API CTimeCounter* GetLastRecvTimer() { return &m_tmLastRecv; };
+	AUTOMALIB_API int GetLastError();
+	AUTOMALIB_API void SetReturnCode(int intReturnCode);
+protected:
+	ProtocolDataType m_DataType;		//資料類別，例如D,M,R,S...
+	unsigned long long m_ulStartAddress;	//開始位址2000,3000...
+	unsigned long m_usAmount;		//資料數量
+	CTimeCounter m_tmLastRecv;
+	int m_intRetCode;
+};
+AUTOMALIB_API class ProtocolWordArray :public ProtocolDataArray
+{
+public:
+	AUTOMALIB_API ProtocolWordArray(
+							unsigned long long usStartAddress,//開始位址2000,3000...
+							unsigned long usAmount);
+	AUTOMALIB_API ~ProtocolWordArray();
+	AUTOMALIB_API WORD GetData(unsigned long long  Addr);
+	AUTOMALIB_API void* GetDataPtr() { return m_pWordData; };
+	AUTOMALIB_API void SetData(unsigned long long  Addr, WORD data);
+protected:
+	WORD *m_pWordData;
+};
+AUTOMALIB_API class ProtocolBitArray :public ProtocolDataArray
+{
+public:
+	AUTOMALIB_API ProtocolBitArray(
+		unsigned long long usStartAddress,//開始位址2000,3000...
+		unsigned long usAmount);
+	AUTOMALIB_API ~ProtocolBitArray();
+	AUTOMALIB_API bool GetData(unsigned long long  Addr);
+	AUTOMALIB_API void SetData(unsigned long long  Addr, bool data);
+protected:
+	
+	bool *m_pBitData;
+
+};
+
+AUTOMALIB_API class MProtocol : public MBase
+{
+public:
+	enum TransStatus {
+		tsClose,
+		tsOpen,
+		tsConnecting,
+		tsIDLE,
+		tsSendRequest,
+		tsWaitResponse,
+		tsError,
+	};
+	enum TransError {
+		teNoError,
+		teTimeOut,
+		teFormatError,
+		teCrcError,
+		teResultError,
+		teUIDError,
+		teFunctionCodeError,
+		teLengthError,
+		teProtocolError,
+		teNotOpen,
+	};
+	AUTOMALIB_API MProtocol(MBase *pB,CString strID,CString strName);
+	AUTOMALIB_API virtual ~MProtocol();
+	AUTOMALIB_API TransStatus GetStatus();
+	AUTOMALIB_API virtual void Reset();
+	AUTOMALIB_API TransError GetLastError();
+protected:
+	TransStatus m_Status;
+	TransError m_LastError;
+};
+}//namespace AutoLib
+
